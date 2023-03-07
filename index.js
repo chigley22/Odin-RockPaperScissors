acf1b8f81ea59c719f6ae3ecb25854f7ed8457d8
Colton Higley <cman_hig@hotmail.com>
2023-03-06 23:41:41 -0700

    Started to update prompt to UI, Added HTML, CSS. Player selection works and computerchoice works.

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
+const buttons = document.querySelectorAll('button')
+const result = document.querySelector('results')
+
+// todo: Connect html buttons and apply to playerselection
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        handleUserSelection(button.id)
+
+        // console.log(playerSelection)
+
+        
+    })
+})
+
+
+function handleUserSelection(playerSelection) {
+    const computerChoice = getComputerChoice()
+    const roundResults = playRound(playerSelection, computerChoice) 
+    console.log(roundResults)
+}
+
+// todo: get computer selection
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 
-// console.log(getComputerChoice())
-
+// todo: display round result
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
+
 
     const winCondition = {
         rock: "Scissors",
         paper: "Rock",
         scissors: "Paper"
     };
+
+
 
     if (playerSelection === computerSelection.toLowerCase()) {
         return "It's a tie!";
@@ -23,34 +47,42 @@ function playRound(playerSelection, computerSelection) {
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
-// console.log(playRound(playerSelection, computerSelection))
 
+// todo: run game until comp or player reaches 5 wins
 function game() {
     let playerScore = 0;
     let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Choose Rock, Paper, or Scissors");
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
+    // for (let i = 0; i < 5; i++) {
+    //     const playerSelection = prompt("Choose Rock, Paper, or Scissors");
+    //     const computerSelection = getComputerChoice();
+    //     const result = playRound(playerSelection, computerSelection);
+    //     console.log(result);
 
-        if(result.startsWith("You Win!")) {
-            playerScore++;
-        } else if(result.startsWith("You Lose!")) {
-            computerScore++;
-        } 
+    //     if(result.startsWith("You Win!")) {
+    //         playerScore++;
+    //     } else if(result.startsWith("You Lose!")) {
+    //         computerScore++;
+    //     } 
         
-    }
 
+
+
+    // }
+
+    
+  // todo: display result
     console.log(`Final Score: You ${playerScore} - ${computerScore} Computer`);
-    if (playerScore > computerScore) {
-        console.log("Congratulations, You Win!")
-    } else if (computerScore > playerScore) {
-        console.log("You've been out smarted by a computer! Please try again!")
-    } else {
-        console.log("It's a Tie!")
-    }
+        if (playerScore > computerScore) {
+    console.log("Congratulations, You Win!")
+        } else if (computerScore > playerScore) {
+    console.log("You've been out smarted by a computer! Please try again!")
+        } else {
+    console.log("It's a Tie!")
 }
 
+
+
+}
 game()
+
